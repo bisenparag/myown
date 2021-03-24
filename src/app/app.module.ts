b0c61f9236ee4ae080e7ebbc298d3ef7fc27fdfc
bisenparag <53506543+bisenparag@users.noreply.github.com>
2021-03-24 19:15:06 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import {IvyCarouselModule} from 'angular-responsive-carousel';
+import { AppComponent } from "./app.component";
+import { IvyCarouselModule } from "angular-responsive-carousel";
+import { CarouselModule } from "ngx-owl-carousel-o";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    IvyCarouselModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, IvyCarouselModule, CarouselModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
